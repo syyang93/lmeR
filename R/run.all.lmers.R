bc1f93e751bdaa4dbe9e666e172ffe245e6b64f8
Stephanie Yang <syang93@jhmi.edu>
2020-07-01 08:53:34 -0400

    Package for running mixed models for multiple variables

---
+++ R/run.all.lmers.R
@@ -0,0 +1,57 @@
+#' Function that will run a regression for your variable of interest across all genes/metabolites/CpGs, in parallel.  Adapted from Vamsee (github.com/vkp3/pillalamarRi)
+#' 
+#' @param tx_expr Expression matrix in form: [genes x samples]. Will be converted to a list(!) of gene-expr vectors (if not input as list)
+#' @param gene.ids Character vector of gene IDs, corresponding to rows in `tx_expr` [genes x samples]
+#' @param cov Regression covariates (fixed effects) [cov x samples]
+#' @param rcov Regression covariates to be treated as random effects
+#' @param SCORE Main covariate to be permuted (not included in `cov`)
+#' @param omit.outlier Whether or not you want to omit gene expression outliers
+#' @param num.cores The number of cores you would like to use
+#' @export 
+#' 
+#' @return All regression coefficients for lmer(gene expression ~ SCORE + cov + (1|rcov)) for all genes
+#' 
+#' @examples
+#' tx_expr <- w.mt[,which(colnames(w.mt) %in% make.names(metablist))]
+#' cov <- w.mt[,which(colnames(w.mt) %in% c('sex', 'age1c', 'PC1', 'PC2', 'PC3', 'bmi', 'Exam'))]
+#' rcov <- w.mt[,which(colnames(w.mt) %in% c('idno'))]
+#' SCORE <- w.mt[,which(colnames(w.mt) == "dpcrAdjMetric")]
+#' lm_res.sort <- run.all.lmers(tx_expr, cov, rcov, colnames(tx_expr), SCORE, omit.outlier = T, num.cores = 10)
+#' 
+#' @note 
+# Can't time test on local machine, since you can't run over multiple cores on your poor laptop.
+# Benchmarked on data with 252 tested metabolites, 8 covariates, 1 random covariate, 1516 total samples on JHPCE with 2 GB. 
+# For loop took 17.83 seconds, pbapply took 2.15 seconds
+
+run.all.lmers <- function(tx_expr, cov, rcov, gene.ids, SCORE, omit.outlier = T, num.cores = 10)
+{
+  require(pbapply)
+  start = Sys.time()
+  lmer.res <-
+    pblapply(tx_expr,            # Expression vector list for `pbapply::pblapply`
+             run_lmer,             # This function
+             cov = cov,           # Covariate matrix (fixed effects), as desribed above
+             rcov = rcov,        # Covariates that are random varaibles
+             SCORE = SCORE,       # PRS
+             omit.outlier = omit.outlier,
+             cl = num.cores)      # Number of cores to parallelize over
+  end = Sys.time()
+  total.time.confint = end-start
+  total.time.confint
+  # total.time.noconfint = end-start
+  
+  lmer.res <- simplify2array(lmer.res, higher=F)
+  
+  rownames(lmer.res) <-
+    c(# 'intercept',
+      'beta',
+      'SE',
+      't_value',
+      'pval',
+      # 'conf.low',
+      # 'conf.high',
+      'corr.rho')
+  colnames(lmer.res) <- gene.ids
+  lm.res <- as.data.frame(t(lmer.res))
+  return(lmer.res)
+}
---
+++ R/run_lmer.R
@@ -0,0 +1,60 @@
+#' A wrapper for deciding either to run a default lm or a two-stage lm.  I always use the default.  Adapted from Vamsee (github.com/vkp3/pillalamarRi)
+#' 
+#' Runs linear regression to estimate effect of SCORE on gene expression/metabolite/epigenetic data
+#' @description formula: Expr ~ SCORE + Cov + (1|Rcov), where Cov = fixed covariates and Rcov = random covariates
+#' 
+#' @param expr Expression vector (numeric) with length = #samples (numeric vector)
+#' @param cov Regression covariates (fixed effects) in form [cov x samples] (data frame)
+#' @param rcov Regression covariates (random effects) in the form [rcov x samples] (data frame)
+#' @param SCORE mtDNA-CN, not included in `cov` (numeric vector)
+#' @param omit.outlier (no longer supported) Whether or not you want to omit gene expression outliers 
+#' @return A [1 x 6] vector output from an lm() like below:
+#'         ['intercept', 'beta', 'SE', 't_value', 'pval', 'corr.rho']
+#' 
+#' Author: Stephanie Yang
+#' Adapted from: Vamsee Pillalamarri
+#' @export
+
+# for testing:
+# w.mt = readRDS('/Volumes/JHPCE/dcs01/active/projects/mesa/syang/R_objects/metab.w.mt.vis1.rds')
+# expr <- w.mt[,which(colnames(w.mt) == "D.Gluconic.acid")]
+# cov <- dplyr::select(w.mt, gender1, age1c, PC1, PC2, PC3, bmi1c)
+# rcov <- dplyr::select(w.mt, race1c)
+# SCORE <- w.mt[,which(colnames(w.mt) == "dpcrAdjMetric")]
+
+run_lmer <- function(expr, cov, rcov, SCORE, omit.outlier = T, outlier_sd = 3) {
+  expr <- as.numeric(expr)
+  
+  # expr <- scale(expr) # uncomment if you would like to scale expression
+  # expr <- inv.norm.transform(expr) # uncomment if you would like to inverse normal transform expression
+  
+  # Create dataframe
+  expr_cov <- cbind(SCORE, expr, cov, rcov)
+
+  # Create equation
+  lmer.form = paste0('expr~SCORE+',paste0(colnames(cov), collapse = '+'), '+', paste0('(1|',colnames(rcov), ')', collapse = '+'))
+  
+  # Run mixed model
+  lmer.fit <- lmer(lmer.form, data = expr_cov)
+  
+  # Get summary
+  lmer.fit.summary <- summary(lmer.fit)
+  
+  # Get corr
+  cor_expr_score <-
+    cor(expr, SCORE)
+  
+  # Capture beta, tval, standard error.
+  lmer.res_ <-
+    as.data.frame(t(coef(lmer.fit.summary)['SCORE',]))
+  
+  # get pval christina's way:
+  pval <- pchisq(lmer.res_$`t value`^2,1,lower.tail=F)
+  
+  lmer.res_ <-
+    cbind(lmer.res_, pval,
+    #      t(confint(lmer.fit)['SCORE', ]),
+          cor_expr_score)
+  
+  return(as.matrix(lmer.res_))
+}
